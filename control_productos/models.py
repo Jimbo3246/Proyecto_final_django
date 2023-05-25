9077ac233e0af89583980f5a8cf5481dda4c60b9
Jimmy Rivera <jimmy.rivera1@unmsm.edu.pe>
2023-05-25 17:17:13 -0500

    Agregado app control_productos con sus modelos y su primera migracion

---
+++ control_productos/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+class Persona(models.Model):
+    nombre=models.CharField(max_length=256)
+    apellido=models.CharField(max_length=256)
+    fecha_nacimiento=models.EmailField()
+    email=models.DateField()
+    telefono=models.CharField(max_length=20)
+    dni=models.CharField(max_length=32)
+    genero=models.CharField(max_length=32)
+    class Meta:
+        abstract=True
+
+class Producto(models.Model):
+    marca=models.CharField(max_length=256)
+    diseno=models.CharField(max_length=256)
+    empaque=models.CharField(max_length=256)
+    categoria=models.CharField(max_length=256)
+    genero=models.CharField(max_length=256)
+    precio_costo = models.DecimalField(max_digits=8, decimal_places=2)
+
+class Cliente(Persona):
+    ciudad=models.CharField(max_length=256)
+    distrito=models.CharField(max_length=256)
+
+class Empleado(Persona):
+    cargo=models.CharField(max_length=256)
+    turno=models.CharField(max_length=32)
+    
