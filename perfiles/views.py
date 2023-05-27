bbefc3ebfc1c567c747d027c3520067f8fb3f9d6
Jimmy Rivera <jimmy.rivera1@unmsm.edu.pe>
2023-05-27 10:51:19 -0500

    Agregado login, logout y mejora en la navegacion

--- perfiles/views.py
+++ perfiles/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import *
 from django.urls import reverse
 from perfiles.forms import UserRegisterForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 def registro(request):
@@ -18,3 +22,25 @@ def registro(request):
         template_name='perfiles/registro.html',
         context={'form':formulario}
     )
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid(): 
+            data = form.cleaned_data
+            usuario = data.get('username')
+            password = data.get('password')
+            user = authenticate(username=usuario, password=password)
+
+            if user:
+                login(request=request, user=user)
+                url_exitosa = reverse('inicio')
+                return redirect(url_exitosa)
+    else:
+        form = AuthenticationForm()
+    return render(
+        request=request,
+        template_name='perfiles/login.html',
+        context={'form':form},
+    )
+class CustomLogoutView(LogoutView):
+    template_name = 'perfiles/logout.html'    
--- sistema_lencimoda/settings.py
+++ sistema_lencimoda/settings.py
@@ -126,8 +126,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-
-
+LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
